daa48e7d4542e5d1861ece2687b192d6007d052b
Pablo Schaffner <pablo@puntorigen.com>
2025-05-21 19:59:25 -0400

    - small bugfix for building

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from brain_proxy.__version__ import __version__
 
 setup(
     name="brain-proxy",
-    version=__version__,
+    version="0.1.48",
     description="OpenAI-compatible FastAPI router with Chroma + LangMem memory.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
